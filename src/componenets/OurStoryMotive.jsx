dc54f71c2cec1eeda5cc39e23c5c2ade79d9944b
NickMP23 <nipittner@gmail.com>
2022-09-03 16:35:25 -0600

    Created Our Story Motive and some CSS

---
+++ src/componenets/OurStoryMotive.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Motive extends Component {
+  render() {
+    return (
+      <div className="OurStoryMotiveContainer">
+        <div className="motiveText">
+          <h1>We’re on a mission to make money more human</h1>
+          <p>
+            Make smart money moves with your personal money coach — always a
+            text away.
+          </p>
+        </div>
+        <div className="altMotiveImgContainer">
+          <img src="https://uploads-ssl.webflow.com/6107e08479e329549563291a/620bef23f589d4a0696e0e79_image-website-hero-20-p-1600.jpeg"></img>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Motive;
