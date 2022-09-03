6328022991eddcc5364b4efb89082021f50a29ad
AndreTheDeveloper <andre.colaris98@live.ca>
2022-09-03 15:09:19 -0600

    new container for as seen in

---
+++ src/componenets/AsSeenIn.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class AsSeenIn extends Component {
+  render() {
+    return (
+      <div className="asSeenInContainer">
+        <div className="asSeenInTitle"></div>
+      </div>
+    );
+  }
+}
+
+export default AsSeenIn;
