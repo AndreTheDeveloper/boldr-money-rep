9c8c8a0216c2ac205da9216ccb73206cbb76ed6a
NickMP23 <nipittner@gmail.com>
2022-09-03 17:35:49 -0600

    Decorating Our Story Font

--- src/componenets/OurStoryMotive.jsx
+++ src/componenets/OurStoryMotive.jsx
@@ -5,7 +5,7 @@ class Motive extends Component {
     return (
       <div className="ourStoryMotiveContainer">
         <div className="ourStoryMotiveText">
-          <h1>We’re on a mission to make money more human</h1>
+          <h1>We’re on a mission to make money more <span className = 'humanHightlight'> human </span></h1>
           <p>
           Before we were a team, we were part of a system in need of change.
           We grew tired of financial jargon, of feeling anxious about our own money, and of looking for help in an industry that thrives off confusion.
