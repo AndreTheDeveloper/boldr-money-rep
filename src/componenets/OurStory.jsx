3001abebc6dca49501793ac1d117ba542c31ddc1
NickMP23 <nipittner@gmail.com>
2022-09-03 16:35:44 -0600

    CSS and Motive Changes

--- src/componenets/OurStory.jsx
+++ src/componenets/OurStory.jsx
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 import NavBar from "./NavBar";
+import OurStoryMotive from "./OurStoryMotive";
 
 class OurStory extends Component {
   render() {
     return (
       <>
         <NavBar />
-        <div className = "ourStoryMotive">
-            <div className = "ourStoryMotiveHeader"><h1>We're on a misson to make money more human</h1></div>
-        </div>
+        <OurStoryMotive />
       </>
     );
   }
