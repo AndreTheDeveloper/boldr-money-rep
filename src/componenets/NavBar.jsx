4dc348e16e0c18f2be8a9008bfe755e5e43ee917
AndreTheDeveloper <andre.colaris98@live.ca>
2022-09-03 17:01:10 -0600

    changed navbar text locations

--- src/componenets/NavBar.jsx
+++ src/componenets/NavBar.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 class NavBar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg bg-light">
         <div className="container-fluid">
           <a className="navbar-brand">
-          <li><NavLink className= "navLink" to=''>Bolder</NavLink></li>
+            <li>
+              <span className="navBarTitle">
+                <NavLink className="navLink" to="">
+                  Bolder
+                </NavLink>
+              </span>
+            </li>
           </a>
           <button
             className="navbar-toggler"
@@ -19,25 +25,39 @@ class NavBar extends Component {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" >
-                <li><NavLink className= "navLink" to='/OurStory'>Our Story</NavLink></li>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link">
-                <li><NavLink className= "navLink" to='/HowItWorks'>How It Works</NavLink></li>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link">
-                <li><NavLink className= "navLink" to='/#NoFilter'>#NoFilter</NavLink></li>
-                </a>
-              </li>
-            </ul>
-          </div>
+          <span className="navBarItems">
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page">
+                    <li>
+                      <NavLink className="navLink" to="/OurStory">
+                        Our Story
+                      </NavLink>
+                    </li>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link">
+                    <li>
+                      <NavLink className="navLink" to="/HowItWorks">
+                        How It Works
+                      </NavLink>
+                    </li>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link">
+                    <li>
+                      <NavLink className="navLink" to="/#NoFilter">
+                        #NoFilter
+                      </NavLink>
+                    </li>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </span>
         </div>
       </nav>
     );
