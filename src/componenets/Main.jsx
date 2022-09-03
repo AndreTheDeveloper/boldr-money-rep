dd6350e94427c2293fe819d340a637e2f29dde43
AndreTheDeveloper <andre.colaris98@live.ca>
2022-09-03 14:49:17 -0600

    motive page text and img

--- src/componenets/Main.jsx
+++ src/componenets/Main.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import NavBar from "./NavBar";
+import Motive from "./Motive";
 
 class Main extends Component {
   render() {
     return (
       <>
         <NavBar />
+        <Motive />
       </>
     );
   }
