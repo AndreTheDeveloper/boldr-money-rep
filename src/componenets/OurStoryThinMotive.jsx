bce995bb328e5f1a6d23e4f5182ed1f1c4d09ffe
NickMP23 <nipittner@gmail.com>
2022-09-03 19:46:16 -0600

    pushing our story changes

---
+++ src/componenets/OurStoryThinMotive.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class OurStoryThinMotive extends Component {
+  render() {
+    return (
+      <div className="ourStoryThinMotiveContainer">
+        <div className="ourStoryThinMotiveText">
+          <h1>We’re on a mission to make money more <span className = 'humanHightlight'> human </span></h1>
+          <p>
+          Before we were a team, we were part of a system in need of change.
+          We grew tired of financial jargon, of feeling anxious about our own money, and of looking for help in an industry that thrives off confusion.
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default OurStoryThinMotive;
