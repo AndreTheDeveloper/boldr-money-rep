0cb2dcd9398ca54c962618d72500e62acf67c764
NickMP23 <nipittner@gmail.com>
2022-09-04 15:15:08 -0600

    splide commit

--- src/componenets/OurStory.jsx
+++ src/componenets/OurStory.jsx
@@ -3,6 +3,7 @@ import NavBar from "./NavBar";
 import OurStoryMotive from "./OurStoryMotive";
 import OurStoryTextSection from "./OurStoryTextSection";
 import OurStoryThinMotive from "./OurStoryThinMotive";
+import OurStoryHumansFirst from "./OurStoryHumansFirst";
 
 class OurStory extends Component {
   render() {
@@ -12,6 +13,7 @@ class OurStory extends Component {
         <OurStoryMotive />
         <OurStoryTextSection />
         <OurStoryThinMotive />
+        <OurStoryHumansFirst/>
       </>
     );
   }
