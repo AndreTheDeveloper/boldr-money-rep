960a0a8aaf5f3023accea686284864701396006e
AndreTheDeveloper <andre.colaris98@live.ca>
2022-09-04 15:47:49 -0600

    employeeDesc done

--- src/componenets/Employees.jsx
+++ src/componenets/Employees.jsx
@@ -20,16 +20,40 @@ class Employees extends Component {
           }}
         >
           <SplideSlide>
-            <div className="employeePic1"></div>
+            <div className="employeePic1">
+              <div className="employeeDesc">
+                Georgi started a side hustle and saved over $15k to buy a van
+                and live the #vanlife
+              </div>
+              <div className="employeeName">GEORGI, DENVER, 29</div>
+            </div>
           </SplideSlide>
           <SplideSlide>
-            <div className="employeePic2"></div>
+            <div className="employeePic2">
+              <div className="employeeDesc">
+                Mal added an addiotional stream of income, began investing and
+                building wealth
+              </div>
+              <div className="employeeName">MAL, CLEVELAND, 35</div>
+            </div>
           </SplideSlide>
           <SplideSlide>
-            <div className="employeePic3"></div>
+            <div className="employeePic3">
+              <div className="employeeDesc">
+                Tracy rebounded from losing her job and paid off over $12k in
+                debt and saved over $20k
+              </div>
+              <div className="employeeName">TRACY, CLEVELAND, 38</div>
+            </div>
           </SplideSlide>
           <SplideSlide>
-            <div className="employeePic4"></div>
+            <div className="employeePic4">
+              <div className="employeeDesc">
+                Trisha expanded her business and saved enough to move to Europe
+                and take time off to travel
+              </div>
+              <div className="employeeName">TRISHA, NEW YORK, 34</div>
+            </div>
           </SplideSlide>
         </Splide>
       </div>
